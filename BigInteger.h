3576e4dd27a04be02bf00a73656123dd10fdd602
alaahassan2022 <57592600+alaahassan2022@users.noreply.github.com>
2019-12-12 18:42:13 +0200

    Update BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -7,7 +7,6 @@ class BigInteger
 public:
 	string multiply(string, string);
 	int equalize(string&, string&);
-	void evenize(string&);
 
 	string subtracte(string, string);
 	string add(string, string);
