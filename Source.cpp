b050213034d987074975ed3a7a08a2e806e2d00a
alaahassan2022 <57592600+alaahassan2022@users.noreply.github.com>
2019-12-16 20:29:42 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,77 +6,79 @@
 using namespace std;
 
 int main() {
-	BigInteger s;
-	ofstream Add;
-	ofstream Subtract;
-	ofstream Multiply;
-	Add.open("Add.txt", ios::app);
-	Subtract.open("Subtract.txt", ios::app);
-	Multiply.open("Multiply.txt", ios::app);
+	//BigInteger s;
+	//ofstream Add;
+	//ofstream Subtract;
+	//ofstream Multiply;
+	//Add.open("AddMytest.txt", ios::app);
+	//Subtract.open("SubtractMytest.txt", ios::app);
+	//Multiply.open("MultiplyMytest.txt", ios::app);
 
-	ifstream AddFile;
-	ifstream SubtractFile;
-	ifstream MultiplyFile;
-	string str;
-	vector<string> file;
+	//ifstream AddFile;
+	//ifstream SubtractFile;
+	//ifstream MultiplyFile;
+	//string str;
+	//vector<string> file;
 
-	////////////////////// ADD
-	AddFile.open("AddTestCases.txt",ios::in);
+	//////////////////////// ADD
+	//AddFile.open("AddTestCases.txt",ios::in);
 
-	while (getline(AddFile, str)) {
-		if (str!="")
-		file.push_back(str);
-	}
-	AddFile.close();
+	//while (getline(AddFile, str)) {
+	//	if (str!="")
+	//	file.push_back(str);
+	//}
+	//AddFile.close();
 
 
-	int index = 1;
-	for (int i = 0; i < stoi(file[0]); i++){
-		Add << s.add(file[index], file[index+1]) << endl << endl;
-		index += 2;
-	}
-	file.clear();
+	//int index = 1;
+	//for (int i = 0; i < stoi(file[0]); i++){
+	//	Add << s.add(file[index], file[index+1]) << endl << endl;
+	//	index += 2;
+	//}
+	//file.clear();
+	///////////////////////////////
+
+	////////////////////////////SUB
+	//SubtractFile.open("SubtractTestCases.txt", ios::in);
+
+	//while (getline(SubtractFile, str)) {
+	//	if (str != "")
+	//		file.push_back(str);
+	//}
+	//SubtractFile.close();
+
+
+	// index = 1;
+	//for (int i = 0; i < stoi(file[0]); i++){
+	//	Subtract << s.subtracte(file[index], file[index + 1]) << endl << endl;
+	//	index += 2;
+	//}
+
+	//file.clear();
 	/////////////////////////////
 
-	//////////////////////////SUB
-	SubtractFile.open("SubtractTestCases.txt", ios::in);
+	///////////////////////Multiply
+	//MultiplyFile.open("MultiplyTestCases.txt", ios::in);
 
-	while (getline(SubtractFile, str)) {
-		if (str != "")
-			file.push_back(str);
-	}
-	SubtractFile.close();
+	//while (getline(MultiplyFile, str)) {
+	//	if (str != "")
+	//		file.push_back(str);
+	//}
+	//MultiplyFile.close();
 
 
-	 index = 1;
-	for (int i = 0; i < stoi(file[0]); i++){
-		Subtract << s.subtracte(file[index], file[index + 1]) << endl << endl;
-		index += 2;
-	}
+	//index = 1;
+	//for (int i = 0; i < stoi(file[0]); i++){
+	//	Multiply << s.multiply(file[index], file[index + 1]) << endl << endl;
+	//	index += 2;
+	//}
 
-	file.clear();
+	//file.clear();
 	///////////////////////////
+	BigInteger s;
 
-	/////////////////////Multiply
-	MultiplyFile.open("MultiplyTestCases.txt", ios::in);
+	cout << s.ModOfPower("3", "18132", "17");
 
-	while (getline(MultiplyFile, str)) {
-		if (str != "")
-			file.push_back(str);
-	}
-	MultiplyFile.close();
-
-
-	index = 1;
-	for (int i = 0; i < stoi(file[0]); i++){
-		Multiply << s.multiply(file[index], file[index + 1]) << endl << endl;
-		index += 2;
-	}
-
-	file.clear();
-	/////////////////////////
-
-	
 
 	return 0;
 }
